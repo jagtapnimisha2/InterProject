37ac31197438af98a1c6305a1ecae7c8091e0aed
Nimisha Jagtap <77167019+jagtapnimisha@users.noreply.github.com>
2023-01-19 10:52:51 +0530

    fiest commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# InterProject
- 
+# Internship project (Avllo)
+# Nimisha Jagtap
+![img1](./im1.png)
+![img2](./im2.png)
+![img3](./im3.png)
+![img4](./im4.png)
+![img5](./im5.png)
+![img6](./im6.png)
+
---
+++ orders.js
@@ -0,0 +1,42 @@
+const Orders = [
+                    { 
+                        productName: 'foldable mini drone',
+                        productNumber: '87654',
+                        paymentStatus: 'due',
+                        shipping: 'pending'
+
+                   },            
+                   { 
+                    productName: 'larvender kf102 drone',
+                    productNumber: '8708',
+                    paymentStatus: 'rrfunded',
+                    shipping: 'decline'
+
+                 },     
+                 { 
+                 productName: 'ruko f11 pro drone',
+                 productNumber: '2784',
+                 paymentStatus: 'due',
+                 shipping: 'pending'
+
+                 },    
+                 { 
+                 productName: 'drone with camera drone',
+                 productNumber: '9824',
+                 paymentStatus: 'paid',
+                 shipping: 'delivered'
+
+                 },            
+                 { 
+                 productName: 'gps 4k drone',
+                 productNumber: '2834',
+                 paymentStatus: 'paid',
+                  shipping: 'deliverd'
+
+   },            
+   
+               
+
+               
+    
+]
